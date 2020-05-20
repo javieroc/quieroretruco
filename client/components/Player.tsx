c4c271d2e0633635115858693e7cc8ebc50a0908
Javier Ocampo <ferroxido@gmail.com>
2020-05-19 23:18:16 -0300

    Add cards to players

--- client/components/Player.tsx
+++ client/components/Player.tsx
@@ -9,9 +9,16 @@ interface Props {
 
 function Player({ data }: Props) {
   return (
-    <div className={`${styles.player} ${styles['player' + data.position]}`}>
-      <img src={`/${data.image}`} className={styles.playerImage} />
-      <h3>Jugador de prueba</h3>
+    <div className={`${styles.playerContainer} ${styles['player' + data.position]}`}>
+      <div className={styles.player}>
+        <img src={`/${data.image}`} className={styles.playerImage} />
+        <h3>Jugador de prueba</h3>
+      </div>
+      <div className={styles.cards}>
+        <img src="/cards/back.png" />
+        <img src="/cards/back.png" />
+        <img src="/cards/back.png" />
+      </div>
     </div>
   )
 }
