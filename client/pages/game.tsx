10447b5b083c80fba51377937d5f8dee9817e46f
Javier Ocampo <ferroxido@gmail.com>
2020-05-20 22:59:35 -0300

    Util to generate random hands

--- client/pages/game.tsx
+++ client/pages/game.tsx
@@ -1,9 +1,12 @@
 import Layout from '../components/Layout'
 import Table from '../components/Table'
+import { generateHands } from '../utils'
 
 const players = ['boxitracio', 'comisario', 'hijitus', 'larguirucho', 'neurus', 'pucho'];
 
 function Game() {
+  const hands = generateHands(players.length)
+
   return (
     <Layout>
       <Table players={players} />
---
+++ client/utils/index.ts
@@ -0,0 +1,16 @@
+const cardsTypes = ['oro', 'copa', 'espada', 'basto']
+const cardsNumbers = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12]
+
+function shuffle(array: string[]): string[] {
+  return array.sort(() => Math.random() - 0.5)
+}
+
+export function generateHands(numOfPlayers: number): string[][] {
+  const cards = cardsTypes.reduce((res, cardType) => [...res, ...cardsNumbers.map((cardNumber) => `${cardNumber}-${cardType}`)], [])
+  const cardsShuffled = shuffle(cards)
+  const res: string[][] = Array(numOfPlayers).fill(0).map((_) => [])
+  for (let i = 1; i <= numOfPlayers * 3; i += 1) {
+    res[i % 6].push(cardsShuffled.shift())
+  }
+  return res
+}
