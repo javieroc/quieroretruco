79b29fa7a72a18900cc633cc6969721723996145
Javier Ocampo <ferroxido@gmail.com>
2020-06-01 00:28:58 -0300

    WIP: home page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -21,14 +21,20 @@ function Home() {
       <div className={styles.home}>
         <Nav />
 
-        <div className={styles.cards}>
-          <img className={styles.card} src="/img/cards/1-espada.png" />
-          <img className={styles.card} src="/img/cards/1-copa.png" />
-          <img className={styles.card} src="/img/cards/1-basto.png" />
-          <img className={styles.card} src="/img/cards/1-oro.png" />
-        </div>
-        <div>
-          <button className={styles.button} onClick={() => setDisplayModal(true)}>Jugar</button>
+        <div className={styles.heroContainer}>
+          <div className={styles.heroText}>
+            <h1>Gran Truco Argentino</h1>
+            <div>
+              <button className={styles.button} onClick={() => setDisplayModal(true)}>Jugar Ahora</button>
+            </div>
+          </div>
+
+          <div className={styles.cards}>
+            <img className={styles.card1} src="/img/cards/1-espada.png" />
+            <img className={styles.card2} src="/img/cards/1-copa.png" />
+            <img className={styles.card3} src="/img/cards/1-basto.png" />
+            <img className={styles.card4} src="/img/cards/1-oro.png" />
+          </div>
         </div>
 
         <Modal show={displayModal} handleModal={handleModal}>
