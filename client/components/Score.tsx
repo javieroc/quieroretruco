34466bc762288285034b4f10acacf5bc36e85040
Javier Ocampo <ferroxido@gmail.com>
2020-06-03 20:35:31 -0300

    WIP: score component

--- client/components/Score.tsx
+++ client/components/Score.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styles from './Score.module.css'
 
 function Score() {
+  const [score, setScore] = useState({ nos: 0, ellos: 0 })
   const canvasRef = React.useRef(null);
 
   function drawLine(context: any, fromX: number, fromY: number, toX: number, toY: number) {
@@ -13,6 +14,28 @@ function Score() {
     context.closePath()
   }
 
+  function drawImage(context: any) {
+    const image = new Image()
+    image.src = '/img/match.png'
+    image.onload = () => {
+      context.drawImage(image, 90, 200, 5, 50)
+    }
+  }
+
+  function drawRotatedImage(context: any, degrees: any) {
+    const image = new Image()
+    const canvas = canvasRef.current
+    image.src = '/img/match.png'
+    image.onload = () => {
+      context.save();
+      context.translate(canvas.width / 2, canvas.height / 2);
+      context.rotate(degrees * Math.PI / 180);
+      context.drawImage(image, -115, -15, 5, 50);
+      //context.drawImage(image, 90, 200, 5, 50)
+      context.restore();
+    }
+  }
+
   useEffect(() => {
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
@@ -20,15 +43,17 @@ function Score() {
     context.lineWidth = 7
     context.lineCap = 'round'
 
-    context.strokeStyle = 'rgba(255,255,255, .7)'
+    context.strokeStyle = 'rgba(255,255,255, .9)'
     drawLine(context, 50, 180, 350, 180)
     drawLine(context, 200, 100, 200, 500)
-    context.fillStyle = 'rgba(255,255,255, .7)'
+    context.fillStyle = 'rgba(255,255,255, .9)'
     context.font = "30px Arial"
     context.textAlign = "center"
     context.fillText("Puntos", canvas.width / 2, 80)
     context.fillText("Nos", 120, 150)
     context.fillText("Ellos", 280, 150)
+    drawImage(context)
+    drawRotatedImage(context, 45)
   }, [])
 
   return (
